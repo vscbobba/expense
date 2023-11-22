59b49ae69768ea7bff9365cd7a3f47902b3b6fe5
vscbobba <vscbobba@gmail.com>
2023-11-22 17:06:31 +0530

    scripting corrections

--- frontend/frontend-script.sh
+++ frontend/frontend-script.sh
@@ -1,14 +1,14 @@
 echo -e "\e[33m install software \e[0m"
 my_log="/tmp/error.log"
 
-dnf install nginx -y &> my_log
+dnf install nginx -y &>$my_log
 rm -rf /usr/share/nginx/html/*
 cp conf /etc/nginx/default.d/expense.conf
 
 echo -e "\e[33m download frontend software \e[0m"
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>> my_log
-cd /usr/share/nginx/html 
+cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>> my_log
 
 echo -e "\e[33m enable and start nginx \e[0m"
